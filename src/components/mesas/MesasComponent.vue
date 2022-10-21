0de05c047ac92ec722311087f76cc87678973bfa
dobleamarilla <41928305+dobleamarilla@users.noreply.github.com>
2022-10-21 15:46:48 +0200

    update

---
+++ src/components/mesas/MesasComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>MESAS SUPER COMPONENT</div>
+</template>
+
+<script>
+export default {
+  name: "MesasComponent",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/CestasView.vue
+++ src/views/CestasView.vue
@@ -12,7 +12,7 @@
     <div class="row" v-for="(_lol, row) in totalRows" v-bind:key="row">
       <template v-for="(_lal, col) in 4" v-bind:key="col">
         <MDBCard
-          v-if="row * 4 + col + 1 < arrayCestas.length"
+          v-if="row * 4 + col + 1 <= arrayCestas.length"
           class="cesta mb-3 ms-2 me-1"
           :class="{
             cestaActiva:
@@ -58,7 +58,12 @@
       </template>
     </div>
   </div>
-  <div class="input-group position-absolute bottom-0 start-0">
+
+  <div v-if="vistaMesas" class="row sizeGeneral">
+    <MesasComponent />
+  </div>
+
+  <div class="position-absolute bottom-0 start-0">
     <button
       @click="volver()"
       class="btn btn-warning ms-2"
@@ -72,6 +77,20 @@
       style="font-size: 27px"
     >
       VISTA MESAS
+    </button>
+    <button
+      @click="nuevaCesta()"
+      class="btn btn-success ms-2"
+      style="font-size: 27px"
+    >
+      CREAR CESTA
+    </button>
+    <button
+      @click="borrarCesta()"
+      class="btn btn-danger ms-2"
+      style="font-size: 27px"
+    >
+      BORRAR CESTA
     </button>
   </div>
 </template>
@@ -89,6 +108,7 @@ import {
 import router from "../router/index";
 import axios from "axios";
 import Swal from "sweetalert2";
+import MesasComponent from "../components/mesas/MesasComponent.vue";
 export default {
   name: "CestasView",
   components: {
@@ -97,6 +117,7 @@ export default {
     MDBCardTitle,
     MDBCardText,
     MDBBtn,
+    MesasComponent,
   },
   setup() {
     const store = useStore();
@@ -108,6 +129,7 @@ export default {
     const indexActivoTrabajador = computed(
       () => store.state.Trabajadores.indexActivo
     );
+    let idCestaSeleccionada = null;
 
     function switchMesas() {
       if (vistaMesas.value) {
@@ -129,6 +151,45 @@ export default {
       );
     }
 
+    function nuevaCesta() {
+      if (
+        indexActivoTrabajador.value != undefined &&
+        indexActivoTrabajador.value != null &&
+        arrayTrabajadores.value
+      ) {
+        axios
+          .post("cestas/crearCesta", {
+            idTrabajador:
+              arrayTrabajadores.value[indexActivoTrabajador.value]._id,
+          })
+          .then((resNuevaCesta) => {
+            if (!resNuevaCesta.data) {
+              throw Error("No se ha podido crear una nueva cesta");
+            }
+          })
+          .catch((err) => {
+            Swal.fire("Oops...", err.message, "error");
+          });
+      }
+    }
+
+    function borrarCesta() {
+      if (idCestaSeleccionada) {
+        axios
+          .post("cestas/fulminarCesta", {
+            idCesta: idCestaSeleccionada,
+          })
+          .then((resBorrarCesta) => {
+            if (!resBorrarCesta.data) {
+              throw Error("No se ha podido borrar la cesta");
+            }
+          })
+          .catch((err) => {
+            Swal.fire("Oops...", err.message, "error");
+          });
+      }
+    }
+
     const totalRows = computed(() => {
       const nCestas = arrayCestas.value.length;
       let nRows = Math.trunc(nCestas / 4);
@@ -143,6 +204,7 @@ export default {
         indexActivoTrabajador.value != null &&
         arrayTrabajadores.value[indexActivoTrabajador.value]
       ) {
+        idCestaSeleccionada = cesta._id;
         axios.post("cestas/cambiarCestaTrabajador", {
           idCesta: cesta._id,
           idTrabajador:
@@ -167,6 +229,8 @@ export default {
       getTotal,
       seleccionar,
       totalRows,
+      nuevaCesta,
+      borrarCesta,
     };
   },
 };
@@ -174,9 +238,10 @@ export default {
 
 <style lang="scss" scoped>
 $anchoCesta: 20rem;
+$alturaCesta: 20rem;
 $alturaSublista: 10rem;
 $alturaGeneral: 43rem;
-$anchoGeneral: 88rem;
+$anchoGeneral: 85rem;
 .switchMesas {
   height: 2.5rem;
   width: calc(4rem + 0.75rem);
@@ -185,6 +250,8 @@ $anchoGeneral: 88rem;
 .cesta {
   min-width: $anchoCesta;
   max-width: $anchoCesta;
+  max-height: $alturaCesta;
+  min-height: $alturaCesta;
 }
 .cestaActiva {
   background-color: rgb(255, 221, 176);
